c0a0cb2fb896bcff7be148e4f0f3fd6a55bcaae1
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-02 22:55:31 +0545

    user verification token

---
+++ src/models/user-verification-token.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import moment from 'moment';
+const Schema = mongoose.Schema;
+ 
+const userVerificationTokenSchema = new mongoose.Schema(
+  {
+      token: {
+        type: Number,
+        required: true,
+      },
+      user_id: { 
+        type: Schema.Types.ObjectId,
+         ref: 'User', 
+         required: true
+        },
+      expires_at: {
+          type: Date,
+          default: moment().add(1, 'h')
+      }
+  },
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  });
+ 
+export default mongoose.model('UserVerificationToken', userVerificationTokenSchema);
