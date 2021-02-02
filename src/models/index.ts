09a3ac04ae7ef9ee3809f2bd29d3b50025a17218
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-02 23:19:37 +0545

    modal index update

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,13 @@
 import Jwt from './json_web_token'
 import User from './user'
 import UserSteps from './user-steps'
+import UserVerificationToken from './user-verification-token'
+import Notification from './notification'
+
 export {
     User,
     Jwt,
-    UserSteps
+    UserSteps,
+    Notification,
+    UserVerificationToken
 }
