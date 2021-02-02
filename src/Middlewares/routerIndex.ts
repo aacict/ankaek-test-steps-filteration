13eafe8d9e3e256c13097b8dac48b8964654d5af
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-02 22:54:09 +0545

    router index middle ware update

--- src/Middlewares/routerIndex.ts
+++ src/Middlewares/routerIndex.ts
@@ -1,16 +1,14 @@
 import {Router} from 'express';
-// import {tokenAuthenticator} from 'express';
-
-// const passport = require('passport');
-// const authRoute = require('../components/Auth/authRoute');
+import verifyjwt from './jwtVerify';
+import authRoute from '../components/Auth/authRoute';
 // const userRoute = require('../components/User/userRoute');
 
 
-module.exports = () => {
-  const router = Router();
-
-  //auth routes added to app router
-//   authRoute(router, passport);
+const router = () => {
+  const router:any = Router();
+  authRoute(router, verifyjwt);
 //   userRoute(router, passport);
   return router;
 };
+
+export default router;
