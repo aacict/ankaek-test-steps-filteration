6ebf045b4a950c749339a0156432397cf0ff5373
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-02 22:51:36 +0545

    routes initialized

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import './config/dbConnection';
-import {config} from 'dotenv';
-
+import router from './middlewares/routerIndex'
 const app: any = express();
 const port: number = 4000;
 
 app.use(express.urlencoded({limit: '50mb', extended: false}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
+// Routes setup
+const apiRoutes: any = router();
+app.use('/', apiRoutes);
+
 app.listen(port, () => {
     try {
         return console.log(`server is listening on ${port}`);
