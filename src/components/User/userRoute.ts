44d34e36ffbb4974ec376d5f555b480f762b0707
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-04 01:02:43 +0545

    user  steps route

--- src/components/User/userRoute.ts
+++ src/components/User/userRoute.ts
@@ -1,11 +1,9 @@
 import UserController from './userController'
 
 const routes = (router, verifyjwt) => {
-  router.post("/steps", verifyjwt, UserController.addUserStep);
+  router.post("/steps", verifyjwt,  UserController.addUserStep);
 
   router.get("/steps", verifyjwt, UserController.getUserSteps);
-
-  router.get("/notify", verifyjwt, UserController.notifyUser);
 
 };
 
