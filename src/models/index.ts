1af8a6d7408143a5794b9f6f8fb9058fededd7ab
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-04 01:03:38 +0545

    router index update

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,13 +1,13 @@
 import Jwt from './json_web_token'
 import User from './user'
-import UserSteps from './user-steps'
+import UserStep from './user-steps'
 import UserVerificationToken from './user-verification-token'
 import Notification from './notification'
 
 export {
     User,
     Jwt,
-    UserSteps,
+    UserStep,
     Notification,
     UserVerificationToken
 }
