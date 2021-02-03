e16f8f9c223af40c570a668d6e9bb5cdc974b9de
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-04 01:04:13 +0545

    user steps changes

--- src/models/user-steps.ts
+++ src/models/user-steps.ts
@@ -1,20 +1,19 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
+import {Schema, model} from 'mongoose';
+// const Schema = mongoose.Schema;
  
-const userStepSchema = new mongoose.Schema(
+const userStepSchema: Schema = new Schema(
   {
-      step: {
-        type: String,
+      steps: {
+        type: Number,
         required: true,
       },
       user_id: { 
         type: Schema.Types.ObjectId,
          ref: 'User', 
-         required: true
         },
   },
   {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
   });
  
-export default mongoose.model('userStep', userStepSchema);
+export default model('userStep', userStepSchema);
