d0c355e2522f0d921625e42ef5f3742fa9b3b075
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-04 01:01:44 +0545

    auth route validations

--- src/components/Auth/AuthRoute.ts
+++ src/components/Auth/AuthRoute.ts
@@ -1,11 +1,12 @@
 import AuthController from './AuthController'
+import validation from '../../helpers/inputValidation'
 
 const routes = (router, verifyjwt) => {
   //sign in route
-  router.post("/sign-in", AuthController.signin);
+  router.post("/sign-in", validation['signInValidation'], AuthController.signin);
 
   //sign up route
-  router.post("/sign-up", AuthController.signup);
+  router.post("/sign-up", validation['signUpValidation'], AuthController.signup);
 
   //log out route
   router.get(
